12c5f778a787e1656e6e7325e72fcfcbf390e069
Jonathan Paz <omahaprogrammer@gmail.com>
2021-12-19 23:22:43 -0600

    Allow find beers and index to permit all requests.

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIntegrationTest.java
@@ -2,15 +2,13 @@ package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class BeerControllerIntegrationTest extends BaseIntegrationTest {
-    @WithMockUser("springuser")
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
@@ -21,7 +19,7 @@ public class BeerControllerIntegrationTest extends BaseIntegrationTest {
 
     @Test
     void findBeersWithHttpBasic() throws Exception {
-        mockMvc.perform(get("/beers/find").with(httpBasic("spring", "guru")))
+        mockMvc.perform(get("/beers/find").with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
