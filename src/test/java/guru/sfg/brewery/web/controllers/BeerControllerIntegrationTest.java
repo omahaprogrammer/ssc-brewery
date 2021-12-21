bf3751d52dc76bb7a80a610f7e80392d723c8c06
Jonathan Paz <omahaprogrammer@gmail.com>
2021-12-20 22:43:33 -0600

    New users

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIntegrationTest.java
@@ -4,11 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class BeerControllerIntegrationTest extends BaseIntegrationTest {
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
@@ -18,7 +27,7 @@ public class BeerControllerIntegrationTest extends BaseIntegrationTest {
     }
 
     @Test
-    void findBeersWithHttpBasic() throws Exception {
+    void findBeersWithAnonymous() throws Exception {
         mockMvc.perform(get("/beers/find").with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
